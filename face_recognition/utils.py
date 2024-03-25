805dd3dcf3411277bed834a94c8fa5924c05d974
Ausbel <ausbel.dev@gmail.com>
2024-03-24 21:56:41 -0600

    Update face_recognition/utils.py with generate_all_embeddings function

--- face_recognition/utils.py
+++ face_recognition/utils.py
@@ -1,15 +1,24 @@
 from deepface import DeepFace
-from database.models import Session
+from database.models import Session, engine
 from database.models import Member, MemberEmbedding
+from sqlalchemy import select
 
 
 # Generates facial embeddings for all images in the database
 def generate_all_embeddings():
     session = Session()
+
+    # members = select(Member)
     members = session.query(Member).all()
     for member in members:
         if member.photo_path is not None:
-            face = DeepFace.represent(str(member.photo_path), model_name="Facenet")
+            print(member.photo_path)
+            photo_path = member.photo_path.split("_")
+            print(photo_path)
+            face = DeepFace.represent(
+                img_path=f"/Users/ausbel/source/python/dl_projects/entrance-checking-system/{photo_path[0]}_ {photo_path[1]}",
+                model_name="Facenet",
+            )
             embedding = face[0]["embedding"]
             new_embedding = MemberEmbedding(
                 member_id=member.id, embedding=str(embedding)
